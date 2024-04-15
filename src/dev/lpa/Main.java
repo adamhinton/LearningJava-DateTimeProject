163d144efa855f57a1e8161afaf5c52db6a612fd
adamd <adam.d.hinton@gmail.com>
2024-04-15 17:29:26 -0500

    Many ways to make Dates

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
+import java.time.LocalDate;
+import java.time.Month;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // yyyy-mm-dd
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+
+        LocalDate Five5 = LocalDate.of(2022, 5, 5);
+        System.out.println(Five5);
+
+        // enums for month, cool
+        LocalDate May5th = LocalDate.of(2022, Month.MAY, 5);
+        System.out.println(May5th);
+
+        LocalDate Day125 = LocalDate.ofYearDay(2022, 125);
+        System.out.println(Day125);
+
     }
 }
